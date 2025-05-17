c7f71f2efbac9391e9165d31f1cdaacda01d8d5b
OscarSume04 <140129614+OscarSume04@users.noreply.github.com>
2025-05-17 18:35:27 -0400

    Adding fugitive & reenable luasnip

--- lua/oscar/plugins/luasnip.lua
+++ lua/oscar/plugins/luasnip.lua
@@ -1,7 +1,7 @@
 return {
     {
-        -- "L3MON4D3/LuaSnip",
-        -- version = "v2.4.0",
-        -- build = "make install_jsregexp",
+        "L3MON4D3/LuaSnip",
+        version = "v2.4.0",
+        build = "make install_jsregexp",
     }
 }
